f06243b475fde66171867c628cd4bd621bb44278
igor.vypov@gmail.com <igorv@Macs-MacBook-Air.local>
2024-02-25 23:20:51 +0300

    compleate 2.4.4, 2.4.5, 2.4.6  start 3.1, 3.2

--- src/HomeWork_2/arrays/Ex2_4.java
+++ src/HomeWork_2/arrays/Ex2_4.java
@@ -14,7 +14,7 @@ public class Ex2_4 {
         return sum;
     }
     static int getMaxEvenElementFromArray(int[] arr){
-        int result = Integer.MIN_VALUE;
+        int result = arr[0];
         for (int i = 0; i < arr.length; i++) {
             if( (i & 1) == 1 && arr[i] > result){
                 result = arr[i];
@@ -23,10 +23,8 @@ public class Ex2_4 {
         return result;
     }
     static String getElementsLessAverage(int[] arr){
-        int sum = 0, numOfElements =0, i =0 ;
-        for(int value: arr){
-            sum += value;
-        }
+        int numOfElements =0, i =0 ;
+        int sum = getSumOfElements(arr);
         int average = sum / arr.length;
         for(int value: arr){
             if(value < average) numOfElements++;
@@ -41,10 +39,44 @@ public class Ex2_4 {
         String strArr = Arrays.toString(result);
         return strArr.substring(1, (strArr.length() - 1));
     }
+    static String getTwoMinElementsFromArray(int[] arr){
+        int a, b;
+        if(arr.length >= 2){
+            Arrays.sort(arr);
+        } else {
+            return "В массиве меньше 2-х элементов";
+        }
+        return arr[0] + ", " + arr[1];
+    }
+
+    static String removeElementsInInterval(int[] arr, int a, int b){
+        int tmp, counter = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] >= a && arr[i] <= b){
+                counter++;
+            }else{
+                arr[i - counter] = arr[i];
+            }
+        }
+        for (int i = arr.length-1; i >= arr.length - counter; i--) {
+            arr[i] = 0;
+        }
+        return Arrays.toString(arr);
+    }
+    static int getSumOfElements(int[] arr){
+        int sum = 0;
+        for (int value: arr){
+            sum += value;
+        }
+        return sum;
+    }
     public static void main(String[] args) {
         int[] container = ArraysUtils.arrayRandom(50,100);
         System.out.println("Сумма четных положительных элементов массива: " + getSumEvenPositiveElements(container));
         System.out.println("Максимальный из элементов массива с четными индексами: " + getMaxEvenElementFromArray(container));
-        System.out.print("Элементы массива, которые меньше среднего арифметического: " + getElementsLessAverage(new int[]{1,2,2,3,4}));
+        System.out.println("Элементы массива, которые меньше среднего арифметического: " + getElementsLessAverage(container));
+        System.out.println("Два наименьших (минимальных) элемента массива: " + getTwoMinElementsFromArray(container));
+        System.out.println("Удалить элементы, принадлежащие интервалу от 5 до 10: " + removeElementsInInterval(container, 5, 10));
+        System.out.println("Сумма цифр массива: " + getSumOfElements(container) );
     }
 }
